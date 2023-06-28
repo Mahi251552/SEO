bc65131760f8af20a5859493012247fb3f273f20
Raj Jat <113715849+Mahi251552@users.noreply.github.com>
2023-06-28 14:45:48 +0530

    Update .cpp

--- .cpp
+++ .cpp
@@ -39,3 +39,67 @@ int main() {
   return 0;
 }
 
+
+
+#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+double get_keyword_density(string text, string keyword) {
+  // Gets the keyword density of a piece of text.
+  regex keyword_regex(keyword);
+  int keyword_count = regex_match(text, keyword_regex).size();
+  int total_word_count = 0;
+  for (char c : text) {
+    if (isalnum(c)) {
+      total_word_count++;
+    }
+  }
+  return (double)keyword_count / total_word_count;
+}
+
+int main() {
+  string text = "This is a piece of text that contains the keyword 'keyword research'.";
+  string keyword = "keyword research";
+  double keyword_density = get_keyword_density(text, keyword);
+  cout << keyword_density << endl;
+
+  return 0;
+}
+
+
+
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+using namespace std;
+
+int get_backlink_count(string domain) {
+  // Gets the backlink count for a domain.
+  CURL *curl;
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.moz.com/linkscape/v1/backlinks/" + domain);
+    curl_easy_setopt(curl, CURLOPT_RETURNTRANSFER, 1);
+    string response = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    // Parses the response and returns the backlink count.
+    json data = json::parse(response);
+    return data["backlinks"];
+  } else {
+    return 0;
+  }
+}
+
+int main() {
+  string domain = "www.example.com";
+  int backlink_count = get_backlink_count(domain);
+  cout << backlink_count << endl;
+
+  return 0;
+}
+
