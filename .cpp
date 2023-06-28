d96bc1b8cc78df5ca0b5d7102cce8ea53654d39a
Raj Jat <113715849+Mahi251552@users.noreply.github.com>
2023-06-28 14:43:57 +0530

    Add files via upload

--- .cpp
+++ .cpp
@@ -1,31 +1,41 @@
 #include <iostream>
 #include <string>
-#include <curl/curl.h>
+#include <vector>
 
 using namespace std;
 
-int get_backlink_count(string domain) {
-  // Gets the backlink count for a domain.
-  CURL *curl;
-  curl = curl_easy_init();
-  if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://api.moz.com/linkscape/v1/backlinks/" + domain);
-    curl_easy_setopt(curl, CURLOPT_RETURNTRANSFER, 1);
-    string response = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-
-    // Parses the response and returns the backlink count.
-    json data = json::parse(response);
-    return data["backlinks"];
-  } else {
-    return 0;
+vector<string> get_keyword_suggestions(string keyword) {
+  // Gets keyword suggestions from Google Keyword Planner.
+  string url = "https://api.google.com/keywordplanner/v1/keywords/autocomplete";
+  string params = "key=YOUR_API_KEY&q=" + keyword;
+  string response = "";
+  try {
+    response = requests::get(url + "?" + params).content;
+  } catch (requests::RequestException e) {
+    cout << "Error: " << e.what() << endl;
+    return {};
   }
+
+  // Parses the response and returns a vector of keyword suggestions.
+  vector<string> suggestions;
+  try {
+    json data = json::parse(response);
+    suggestions = data["suggestions"];
+  } catch (json::exception e) {
+    cout << "Error: " << e.what() << endl;
+    return {};
+  }
+
+  return suggestions;
 }
 
 int main() {
-  string domain = "www.example.com";
-  int backlink_count = get_backlink_count(domain);
-  cout << backlink_count << endl;
+  string keyword = "keyword research";
+  vector<string> suggestions = get_keyword_suggestions(keyword);
+  for (string suggestion : suggestions) {
+    cout << suggestion << endl;
+  }
 
   return 0;
 }
+
